9b254f8dac4f2ee2680e3f704a9a7367a18241e7
Rahul Kadam <kadamrahulj0909@gmail.com>
2021-08-11 14:15:15 +0530

    Update decrypt_secret.sh

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -2,4 +2,3 @@ mkdir $HOME/secrets
 echo $HOME
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
 --output $HOME/secrets/secrets.json secrets.json.gpg
-git update-index --chmod=+x ./.github/scripts/decrypt_secret.sh
